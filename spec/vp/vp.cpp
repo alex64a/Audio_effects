4516389ba026e5779e620c8a68292b5b162b05ca
alex64a <aleksamitrovcan@gmail.com>
2024-02-14 12:30:31 +0100

    Updated vp

--- spec/vp/vp.cpp
+++ spec/vp/vp.cpp
@@ -5,19 +5,25 @@
 #include "dma.hpp"
 #include "filter.hpp"
 #include "timer.hpp"
-
+#include <iostream>
 using namespace std;
 using namespace sc_core;
 using namespace tlm;
 
 vp::vp(sc_module_name name) : sc_module(name), gen(NULL), ic(NULL), mem(NULL), dm(NULL), filter_ip(NULL), t(NULL)
 {
+
+
   gen = new generator("m_generator");
   ic = new interconnect("m_interconnect");
   mem = new memory("m_memory");
   dm = new dma("m_dma");
   filter_ip = new filter("m_filter");
   t = new timer("m_timer");
+  
+  s_gen.register_b_transport(this, &vp::b_transport);
+//	s_cpu.register_b_transport(this, &vp::b_transport);
+
 
   t->soc.bind(ic->s_timer);
   gen->isoc.bind(ic->s_gen);
@@ -28,6 +34,17 @@ vp::vp(sc_module_name name) : sc_module(name), gen(NULL), ic(NULL), mem(NULL), d
   dm->dma_isoc(mem->mem_tsoc);
   dm->wr_port(*filter_ip);
   dm->rd_port(*filter_ip);
+tlm_global_quantum::instance().set(sc_time(10, SC_NS));
 
-  tlm_global_quantum::instance().set(sc_time(10, SC_NS));
+SC_REPORT_INFO("VP", "Platform is constructed");
+}
+
+
+void vp::b_transport(pl_t& pl, sc_time& delay){
+
+
+  s_interconnect->b_transport(pl,delay);
+  SC_REPORT_INFO("VP", "Transaction passes...");
+
+
 }
